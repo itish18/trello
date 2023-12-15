fabcec9910708b1b91b44fe69f192459b332c9e9
itish18 <itish.v007@gmail.com>
2023-12-15 17:29:22 +0530

    final

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,8 +1,9 @@
-import { auth } from "@clerk/nextjs";
-import { OrgControl } from "./_components/org-control";
 import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs";
 
-export async function generateMetaData() {
+import { OrgControl } from "./_components/org-control";
+
+export async function generateMetadata() {
   const { orgSlug } = auth();
 
   return {
@@ -10,15 +11,13 @@ export async function generateMetaData() {
   };
 }
 
-export default function OrganizationIdLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <OrgControl />
       {children}
     </>
   );
-}
+};
+
+export default OrganizationIdLayout;
